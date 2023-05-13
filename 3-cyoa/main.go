af173ab031df75e0879acad89ad85b0dbefc6b95
Ahmed Abd El-Gawad <ahmedabdelgowadramadan@gmail.com>
2023-05-13 12:42:44 +0300

    Implement Task #3

--- 3-cyoa/main.go
+++ 3-cyoa/main.go
@@ -4,6 +4,8 @@ import (
 	"encoding/json"
 	"flag"
 	"fmt"
+	"html/template"
+	"net/http"
 	"os"
 )
 
@@ -16,6 +18,37 @@ type Story map[string]struct {
 	} `json:"options"`
 }
 
+type StoryMux struct {
+	story Story
+}
+
+func (m StoryMux) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	storyArc := r.URL.Query().Get("arc")
+	if storyArc == "" {
+		storyArc = "intro" // start begining of story
+	}
+
+	arc, ok := m.story[storyArc]
+	if !ok {
+		http.Error(w, fmt.Sprintf("arc not found %s", storyArc), http.StatusNotFound)
+		return
+	}
+
+	temp, err := template.ParseFiles("arc.html")
+	if err != nil {
+		http.Error(w, fmt.Sprintf("failed to parse template, %v", err), http.StatusInternalServerError)
+	}
+
+	if err := temp.Execute(w, arc); err != nil {
+		http.Error(w, fmt.Sprintf("failed to execute template, %v", err), http.StatusInternalServerError)
+	}
+}
+
+func NewstoryMux(story Story) http.Handler {
+	return StoryMux{story}
+
+}
+
 func main() {
 	jsonFileName := flag.String("json", "gopher.json", "path of a json file that represents the story")
 	flag.Parse()
@@ -24,16 +57,16 @@ func main() {
 
 	if err != nil {
 		fmt.Printf("Failed to open %s: %v", *jsonFileName, err)
-		return
 	}
 	defer file.Close()
 
 	var story Story
 	if err := json.NewDecoder(file).Decode(&story); err != nil {
 		fmt.Printf("Failed to decode %s: %v", *jsonFileName, err)
-		return
 	}
 
 	fmt.Printf("%+v\n", story)
 
+	mux := NewstoryMux(story)
+	http.ListenAndServe(":8080", mux)
 }
